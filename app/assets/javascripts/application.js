ed6de2649fd25d9e09978ebc1282d5365130f271
tatyana_pol <tatyana_pol@yahoo.com>
2016-10-25 04:11:44 +0000

    M

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require ckeditor-jquery
 //= require turbolinks
 //= require_tree .
+
+/* global $ */
+$('.ckeditor').ckeditor({
+  // optional config
+});
