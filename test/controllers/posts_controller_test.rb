bc6e2194b5de124fc9516d17712a12bc88e34fd3
tatyana_pol <tatyana_pol@yahoo.com>
2017-06-16 21:44:16 +0000

    M

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -17,7 +17,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create post" do
     assert_difference('Post.count') do
-      post posts_url, params: { post: { body: @post.body, title: @post.title } }
+      post posts_url, params: { post: { body: @post.body, body: @post.body, body: @post.body, g: @post.g, g: @post.g, post: @post.post, post: @post.post, scaffold: @post.scaffold, scaffold: @post.scaffold, title: @post.title, title: @post.title, title: @post.title } }
     end
 
     assert_redirected_to post_url(Post.last)
@@ -34,7 +34,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update post" do
-    patch post_url(@post), params: { post: { body: @post.body, title: @post.title } }
+    patch post_url(@post), params: { post: { body: @post.body, body: @post.body, body: @post.body, g: @post.g, g: @post.g, post: @post.post, post: @post.post, scaffold: @post.scaffold, scaffold: @post.scaffold, title: @post.title, title: @post.title, title: @post.title } }
     assert_redirected_to post_url(@post)
   end
 
